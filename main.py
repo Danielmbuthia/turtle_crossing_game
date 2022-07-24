ab224bdccb4ef0ac32594f4820ed4b01256d50ac
Daniel Mbuthia <dambuthia@outlook.com>
2022-07-24 23:53:19 +0300

    player implementation done

--- main.py
+++ main.py
@@ -4,11 +4,22 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# player
+player = Player()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(fun=player.move_up, key="Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # detect if player reach the top
+    if player.ycor() == player.finish_line_y:
+        player.increase_level()
+        player.reset_player()
--- player.py
+++ player.py
@@ -1,7 +1,25 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.finish_line_y = FINISH_LINE_Y
+        self.shape('turtle')
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+        self.level = 1
+
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def increase_level(self):
+        self.level += 1
+
+    def reset_player(self):
+        self.goto(STARTING_POSITION)
